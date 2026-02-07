289b9374d10036a7b5a28ec0e260dfb23193f2e8
Kevin Yang <kyang93.charles@gmail.com>
2026-02-07 00:08:02 -0500

    timeline update

--- src/data/news.ts
+++ src/data/news.ts
@@ -10,10 +10,6 @@ export const news: NewsItem[] = [
     content: "The start of my journey in research...",
   },
   {
-    date: "2024-09",
-    content: "Began studying at Brown!",
-  },
-  {
     date: "2022-06",
     content: "Obtained the title of the 6th Professional Go Player in North America",
   },
